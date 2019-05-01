61771b1455d822459bdb0586d6db815af7b8d3fa
Lu Weizheng <masterstevelu>
2019-05-02 07:15:03 +0800

    multiprocessing

--- python/acc/benchmark.py
+++ python/acc/benchmark.py
@@ -4,6 +4,7 @@ from timeit import timeit
 import timeit, functools
 import numpy as np
 import numba
+import multiprocessing
 
 # 计算500期的移动均线，并将结果保存到一个列表里返回
 def ma_basic(data, ma_length):
@@ -29,7 +30,21 @@ def ma_basic(data, ma_length):
             sum_tick += tick
         ma.append(sum_tick/ma_length)
 
-    # 返回数据
+    return ma
+
+def ma_numpy_wrong(data, ma_length):
+    ma = []
+    data_window = data[:ma_length]
+    test_data = data[ma_length:]
+
+    for new_tick in test_data:
+        data_window.pop(0)
+        data_window.append(new_tick)
+
+        # 使用numpy求均值，注意这里本质上每次循环都在创建一个新的numpy数组对象，开销很大
+        data_array = np.array(data_window)
+        ma.append(data_array.mean())
+
     return ma
 
 def ma_numpy_right(data, ma_length):
@@ -106,7 +121,19 @@ if __name__ == "__main__" :
     t_numba = timeit.Timer(functools.partial(ma_numba, data, ma_length))
     t_cache_algorithm = timeit.Timer(functools.partial(ma_cache, data, ma_length))
 
-    print("basic : " + str(t_basic.timeit(test_times)))
+    # print("basic : " + str(t_basic.timeit(test_times)))
     print("numpy right : " + str(t_numpy_right.timeit(test_times)))
-    print("numba : " + str(t_numba.timeit(test_times)))
-    print("cache algorithm :" + str(t_cache_algorithm.timeit(test_times)))
+    # print("numba : " + str(t_numba.timeit(test_times)))
+    # print("cache algorithm :" + str(t_cache_algorithm.timeit(test_times)))
+
+    start = time.time()
+    cores = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=cores)
+
+    for j in range(test_times):
+        pool.apply_async(ma_numpy_right, (data, ma_length))
+
+    pool.close()
+    pool.join()
+    elapsed = time.time() - start
+    print("time : " + str(elapsed))
